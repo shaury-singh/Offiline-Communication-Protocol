1a819c836a9821d3ca72c2f4b0a2e00e5e33a692
shaury-singh <rathore.singh.shaury@gmail.com>
2025-12-09 04:53:38 +0530

    initial handshake

--- protocol.cpp
+++ protocol.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <vector>
 #include <ctime>
+#include <random>
 #include "encryption.h"
 #include "scheduling.h"
 
@@ -11,6 +12,19 @@ long long generateISN(){
     srand(time(0));
     long long random = rand() << 18;
     return random;
+}
+
+string generateRandomString(int length) {
+    const std::string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(0, letters.size() - 1);
+    string result;
+    result.reserve(length);
+    for (int i = 0; i < length; i++) {
+        result += letters[dist(gen)];
+    }
+    return result;
 }
 
 class Sender{
@@ -26,40 +40,32 @@ class Sender{
             string location;
             string deviceID;
         };
-        struct payload{
+        struct info{
             string walletID;
             string userID;
             string stateCode;
             string IEC;
         };
-        struct SYNPacket{
-            int SYN;
-            string EncryptionCode;
-        };
         header head;
-        payload payloadData;
+        info infoData;
         recieverInfo rInfo;
-        SYNPacket SYNPckt;
-        bool SYNSent;
-        bool recieveACKMsg;
-        bool sendSYNACK;
-        // int SYN;
+        bool initialHandshake = false;
+        bool SYNSent = false;
+        bool recieveACKMsg = false;
+        bool sendSYNACK = false;
+        int SYN;
         int ACK;
         int SYNACK;
-        vector<vector<string>> hexMatrix;
-        vector<vector<string>> textMapping;
-        vector<vector<string>> roundKeys;
-        vector<vector<string>> encrypt;
         vector<vector<int>> fixedMatrix = {{1,2,0,0},{1,3,1,1},{1,0,1,1},{1,0,0,0}};
     public:
         Sender(long long ISN, string walletID, string userID, string location, string deviceID, string stateCode, string IEC){
             this -> head.ISN = ISN;
             this -> head.location = location;
             this -> head.deviceID = deviceID;
-            this -> payloadData.walletID = walletID;
-            this -> payloadData.userID = userID;
-            this -> payloadData.stateCode = stateCode;
-            this -> payloadData.IEC = IEC;
+            this -> infoData.walletID = walletID;
+            this -> infoData.userID = userID;
+            this -> infoData.stateCode = stateCode;
+            this -> infoData.IEC = IEC;
         }
         void setRecieverInfo(string encryptionCode, string walletId, string userId, string stateCode){
             this -> rInfo.encryptionCode = encryptionCode;
@@ -67,16 +73,15 @@ class Sender{
             this -> rInfo.userId = userId;
             this -> rInfo.stateCode = stateCode;
         }
-        SYNPacket sendSYN(){
+        int sendSYN(){
             int SYN = generateISN();
-            this -> SYNPckt.SYN = SYN;
-            this -> SYNPckt.EncryptionCode = this -> payloadData.IEC;
+            this -> SYN = SYN;
             SYNSent = true;
-            cout << "SYN Sent to WalletID: " << this -> rInfo.walletId << " SYN : " << this -> SYNPckt.SYN << endl;
-            return SYNPckt;
+            cout << "SYN Sent to WalletID: " << this -> rInfo.walletId << " SYN : " << this -> SYN << endl;
+            return SYN;
         }
         bool recieveACK(int ACK){
-            if (SYNSent && ACK == this -> SYNPckt.SYN + 1){
+            if (SYNSent && ACK == SYN + 1){
                 this -> ACK = ACK;
                 recieveACKMsg = true;
                 return true;
@@ -85,18 +90,54 @@ class Sender{
             return false;
         }
         int sendSYN_ACK(){
-            this -> SYNACK = this -> SYNPckt.SYN + 2;
+            this -> SYNACK = SYN+2;
             this -> sendSYNACK = true;
             return this -> SYNACK;
         }
-        vector<vector<string>> sendData(string data){
-            this -> hexMatrix = string_to_hex_(this -> payloadData.IEC);
-            this -> textMapping = string_to_hex_(data);
-            unordered_map<string,string> sBox = get_sbox();
-            this -> roundKeys = generateRoundKeys_(hexMatrix,sBox);
-            this -> encrypt = encryptMessage_(fixedMatrix,roundKeys,textMapping,0);
-            return this -> encrypt;
+        string generateEncryptionKey(string senderKey, string receiverKey){
+            string encryptionKey = "";
+            int i=0;
+            int j=1;
+            while (i<senderKey.length()){
+                encryptionKey = encryptionKey + senderKey[i];
+                i+=2;
+            }
+            while (j<receiverKey.length()){
+                encryptionKey = encryptionKey + receiverKey[j];
+                j+=2;
+            }
+            return encryptionKey;
         }
+        vector<vector<string>> sendEncryptionKey(){
+            vector<vector<string>> encrypt;
+            string encryptKey = generateEncryptionKey(this->infoData.IEC,this->rInfo.encryptionCode);
+            if (initialHandshake){
+                string bundleKey = generateRandomString(16);
+                vector<vector<string>> hexMatrix = string_to_hex_(encryptKey);
+                vector<vector<string>> textMapping = string_to_hex_(bundleKey);
+                unordered_map<string,string> sBox = get_sbox();
+                vector<vector<string>> roundKeys = generateRoundKeys_(hexMatrix,sBox);
+                encrypt = encryptMessage_(fixedMatrix,roundKeys,textMapping,0);
+                this->infoData.IEC = generateEncryptionKey(encryptKey,bundleKey);
+            } else {
+                vector<vector<string>> hexMatrix = string_to_hex_(this->rInfo.encryptionCode);
+                vector<vector<string>> textMapping = string_to_hex_(this->infoData.IEC);
+                unordered_map<string,string> sBox = get_sbox();
+                vector<vector<string>> roundKeys = generateRoundKeys_(hexMatrix,sBox);
+                encrypt = encryptMessage_(fixedMatrix,roundKeys,textMapping,0);
+                this -> infoData.IEC = encryptKey;
+            }
+            return encrypt;
+        }
+        // vector<vector<string>> sendData(string data){
+        //     while (!this->SYNSent || !this->recieveACKMsg || !this->sendSYNACK);
+        //     this -> hexMatrix = string_to_hex_(this -> payloadData.IEC);
+        //     this -> textMapping = string_to_hex_(data);
+        //     unordered_map<string,string> sBox = get_sbox();
+        //     this -> roundKeys = generateRoundKeys_(hexMatrix,sBox);
+        //     this -> encrypt = encryptMessage_(fixedMatrix,roundKeys,textMapping,0);
+        //     return this -> encrypt;
+        // }
 };
 
 class Reciever{
@@ -163,15 +204,17 @@ int main(){
     int ISN = generateISN();
     Sender sender(ISN,"12345","12345","New Delhi","12345","Hello","Thats my Kung Fu");
     sender.setRecieverInfo("misunderstanding","23456","23456","Shaury");
-    sender.sendSYN();
-    sender.recieveACK(ISN);
-    sender.sendSYN_ACK();
-    vector<vector<string>> data = sender.sendData("Thats my Kung Fu");
-    for (int i=0; i<data.size(); i++){
-        for (int j=0; j<data[i].size(); j++){
-            cout << data[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // cout << generateRandomString(16);
+    // cout << sender.encryptionKey("Thats my Kung Fu","misunderstanding");
+    // int SYN = sender.sendSYN();
+    // sender.recieveACK(ISN);
+    // sender.sendSYN_ACK();
+    // vector<vector<string>> data = sender.sendData("Thats my Kung Fu");
+    // for (int i=0; i<data.size(); i++){
+    //     for (int j=0; j<data[i].size(); j++){
+    //         cout << data[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
     return 0;
 }
